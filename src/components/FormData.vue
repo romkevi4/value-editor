894ebe978bd2740bd70de1497adb02098c75f8f4
Roman Shatskikh <shatskikh.roman@gmail.com>
2023-07-19 16:51:05 +0300

    feat: finish logic of work

--- src/components/FormData.vue
+++ src/components/FormData.vue
@@ -7,14 +7,13 @@
 
         <select
             name="element"
-            class="mb-4 py-2 px-3 bg-transparent border border-gray-500 focus:border-blue-500 focus:outline-none rounded-md cursor-pointer font-Inter"
+            class="mb-4 py-2 px-3 bg-transparent border border-black focus:border-blue-500 focus:outline-none rounded-md cursor-pointer font-Inter"
             v-model="selectedItemValue"
         >
             <option value="" autofocus>Выберите элемент</option>
             <option
-                :value="item"
-                :key="item.id"
                 v-for="(item) in items"
+                v-bind:value="item.value"
             >
               {{ item.name }}
             </option>
@@ -25,7 +24,7 @@
             type="text"
             class="mb-6 py-2 px-4 w-25 border outline-0 focus:ring-1 focus:ring-blue-500 rounded-md font-Inter"
             id="elementsInput"
-            :value="selectedItemValue.value"
+            :value="selectedItemValue"
             @input="handleInput"
         >
 
@@ -41,37 +40,34 @@
 <script>
     import { initialData } from '../utils/initialData';
 
+    const newData = initialData.map((item, i) => ({
+        ...item,
+        _id: i
+    }));
+
     export default {
         data() {
-            const newData = initialData.map((item, i) => ({
-              ...item,
-              id: i
-            }));
-
             return {
                 items: newData,
                 selectedItemValue: '',
-                selectedItemId: undefined
+                selectedItemId: undefined,
+                changedValue: ''
             };
         },
         methods: {
             handleInput(evt) {
-              console.log(evt.target.value);
-              this.selectedItemValue = evt.target.value;
-              this.selectedItemId = evt.target.value.id;
+                const selectedItem = this.items.find(item => item.value === this.selectedItemValue);
+                selectedItem && (this.selectedItemId = selectedItem._id);
+                this.changedValue = evt.target.value;
             },
 
             handleSubmit(evt) {
                 evt.preventDefault();
-
-                console.log(this.selectedItemId);
-                const selectedItem = this.items.find(item => item.id === this.selectedItemId);
-                // console.log(selectedItem);
-                // console.log(this.selectedItemValue);
-
-                selectedItem && (selectedItem.value = this.selectedItemValue);
+                const selectedItem = this.items.find(item => item._id === this.selectedItemId);
+                selectedItem && (selectedItem.value = this.changedValue);
                 this.selectedItemValue = '';
                 this.selectedItemId = undefined;
+                this.changedValue='';
             }
         },
     };
